719a18651cca8527bcd1d26c6d81c3f447c7326f
KrisYounger <779378-KrisYounger@users.noreply.replit.com>
2023-05-03 19:47:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+
+/*
+ * 
+ * 1. Declare a madLib variable
+ * 2. Use the adjective1, adjective2, and adjective3 variables to set the madLib variable to the message:
+ * 
+ * 'The Intro to JavaScript course is amazing. James and Julia are so fun. I cannot wait to work through the rest of this entertaining content!'
+ */
+
+let adjective1 = "amazing";
+let adjective2 = "smart";
+let adjective3 = "entertaining";
+
+
+let madLib1 =  "The ZipCode Prep course is " + adjective1 + ". Tyrell is so " + adjective2 + ". I cannot wait to work through the rest of this " + adjective3 + " content!";
+
+console.log("");
+console.log("First MadLib");
+console.log("");
+console.log(madLib1);
+
+console.log("");
+console.log("Second MadLib");
+console.log("");
+
+let animal1 = "amazing";
+let food1 = "smart";
+let adjective4 = "entertaining";
+
+
+let madLib2 =  "The " + animal1 + " ate all the " + food1 + ". Watching was very " + adjective3 + "!";
+console.log(madLib2);
+
+/* NOW, make your own, using three words, and a template string that makes a funny string.*/
+
+let animal2 = "";
+let food3 = "";
+let color1 = "";
+let fruit1 = "";
+
+let madLib3 =  "Yesterday I saw a " + animal1 + " eating " + food1 + ". The " + animal2 + " was wearing " + color1 + "pajamas and had a "+ fruit1+ "on its head!";
+console.log(madLib3);
+
+// NOW change the four words and see the new, different phrase.
+
+// Copy the code above and make your own madlibs sentence.
+
